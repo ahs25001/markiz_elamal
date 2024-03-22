af75fb110693d1860379a1f57ecef638118bb75b
Ahmed Abdelhamid <ahmedabdelhamid887@gmail.com>
2024-03-22 14:55:21 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/markiz_elamal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.markiz_elamal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:markiz_elamal/features/splash_feature/presentation/splash_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Markiz Elamal',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SplashView(),
+    );
+  }
+}
